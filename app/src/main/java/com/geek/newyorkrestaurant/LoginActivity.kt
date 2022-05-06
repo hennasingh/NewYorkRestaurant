9c7736d72c4777c90779e3a7f2e001f1257c5e42
henna_dev <hennasingh04@gmail.com>
2022-05-06 19:50:49 +0100

    added Realm dependencies, implemented loginActivity and application subclass to initiate realm SDK

---
+++ app/src/main/java/com/geek/newyorkrestaurant/LoginActivity.kt
@@ -0,0 +1,95 @@
+package com.geek.newyorkrestaurant
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.geek.newyorkrestaurant.databinding.ActivityLoginBinding
+import io.realm.mongodb.Credentials
+import io.realm.mongodb.User
+import timber.log.Timber
+
+class LoginActivity : AppCompatActivity() {
+
+    private var user: User? = null
+    private lateinit var binding: ActivityLoginBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+      binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        user = newYorkApp.currentUser()
+
+        if(user != null){
+            startActivity(Intent(this, NewYorkActivity::class.java))
+        }
+
+        binding.buttonLogin.setOnClickListener{
+            login(false)
+        }
+        binding.buttonCreate.setOnClickListener{
+            login(true)
+        }
+    }
+
+    private fun login(createUser: Boolean) {
+
+        if(!validateCredentials()){
+            onLoginFailed("Fields cannot be empty")
+            return
+        }
+
+        // while this operation completes, disable the buttons to login or create a new account
+        binding.buttonLogin.isEnabled = false
+        binding.buttonCreate.isEnabled = false
+
+        val email = binding.username.text.toString()
+        val password = binding.password.text.toString()
+
+
+        if(createUser){
+            //register  a user
+            newYorkApp.emailPassword.registerUserAsync(email, password){
+                if(it.isSuccess){
+                    Timber.d("User successfully registered")
+                    // when the account has been created successfully, log in to the account
+                    login(false)
+                }else {
+                    onLoginFailed(it.error.errorMessage ?: "An error occurred on registering")
+                    enableButtons()
+                }
+            }
+
+        } else {
+            val credentials = Credentials.emailPassword(email, password)
+            newYorkApp.loginAsync(credentials){
+                if(!it.isSuccess){
+                    enableButtons()
+                    onLoginFailed(it.error.errorMessage ?: "An error occurred")
+                } else {
+                    startActivity(Intent(this, NewYorkActivity::class.java))
+                }
+            }
+        }
+    }
+
+    private fun enableButtons() {
+        binding.buttonCreate.isEnabled = true
+        binding.buttonLogin.isEnabled = true
+    }
+
+    private fun onLoginFailed(errorMsg: String) {
+
+        Timber.e(errorMsg)
+        Toast.makeText(baseContext, errorMsg, Toast.LENGTH_LONG).show()
+    }
+
+    private fun validateCredentials(): Boolean = when {
+        // zero-length usernames and passwords are not valid (or secure), so prevent users from creating accounts with those client-side.
+        binding.username.text.toString().isEmpty() -> false
+        binding.username.text.toString().isEmpty() -> false
+        else -> true
+    }
+}
---
+++ app/src/main/java/com/geek/newyorkrestaurant/NewYorkRestaurantApp.kt
@@ -0,0 +1,21 @@
+package com.geek.newyorkrestaurant
+
+import android.app.Application
+import io.realm.Realm
+import io.realm.mongodb.App
+import io.realm.mongodb.AppConfiguration
+import timber.log.Timber
+
+const val appId = "newyorkrestaurants-ruyhm"
+lateinit var newYorkApp: App
+class NewYorkRestaurantApp : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+
+        Timber.plant(Timber.DebugTree())
+
+        newYorkApp = App(AppConfiguration.Builder(appId).build())
+    }
+}
